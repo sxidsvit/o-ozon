ac8d221ea5f0f655fa54766ba3c528e810eed451
unknown <s.p.antonyuk@gmail.com>
2019-08-17 01:18:37 +0300

    Catalog activ atem styled & filter-title changed

--- modules/renderCatalog.js
+++ modules/renderCatalog.js
@@ -4,9 +4,10 @@ export default function renderCatalog() {
   const catalogWrapper = document.querySelector('.catalog') // обертка списка категорий каталога
   const catalogList = document.querySelector('.catalog-list') // тэг ul для вывода списка категорий каталога
   const categories = new Set() // коллекция для сохранения уникальных значений категорий
+  const filterTitle = document.querySelector('.filter-title h5')
 
   cards.forEach(card => { // получаем все категории товаров
-    categories.add(card.dataset.category) // получаем значение дата-атрибута data-category
+    categories.add(card.dataset.category) // получаем значение дата-атрибута data-category и добавляем его в  коллекцию
   })
 
   categories.forEach((item) => { // формируем список категорий
@@ -14,6 +15,8 @@ export default function renderCatalog() {
     li.textContent = item
     catalogList.appendChild(li)
   })
+
+  const allLi = catalogList.querySelectorAll('li')
 
   catalogBtn.addEventListener('click', (event) => {
     if (catalogWrapper.style.display) { // Отображение/скрытие списка категорий
@@ -31,6 +34,14 @@ export default function renderCatalog() {
           card.setAttribute('category-filter', 'no')
         }
       })
+      allLi.forEach((elem) => {
+        if (elem === event.target) {
+          elem.classList.add('active')
+        } else {
+          elem.classList.remove('active')
+        }
+      })
+      filterTitle.textContent = event.target.textContent
     }
   })
 
