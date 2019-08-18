07826399ef2dbca8f529b0a9d0dbc86d196b1fd5
unknown <s.p.antonyuk@gmail.com>
2019-08-18 07:32:12 +0300

    sourse-map in webpack.config.js created

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path')
+
+module.exports = {
+  // entry: path.join(__dirname, '/src/index.js'),
+  // output: {
+  //   path: path.resolve(__dirname, '/dist'),
+  //   publicPath: '/dist/',
+  //   filename: 'main.js'
+  // },
+  devtool: 'source-map'
+}
